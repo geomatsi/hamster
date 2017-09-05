ed3b389b4c74e5e9d83f585eb77a6b23adaf2ee0
Sergey Matyukevich <geomatsi@gmail.com>
2017-09-05 23:10:33 +0300

    hamster: use channel 110 by default
    Channel 110 is 2400 + 110 = 2510MHz, which is beyond WiFi
    2.4G frequency range. It turned out that those channels
    are much more friendly for nRF24L01+/PA/LNA modules.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- include/hamster.h
+++ include/hamster.h
@@ -6,7 +6,7 @@
 /* radio settings */
 
 #define NRF_ADDR	{ 0xE1, 0xE1, 0xE1, 0xE1, 0xE1 }
-#define NRF_CHAN	10
+#define NRF_CHAN	110
 #define NRF_RATE	RF24_RATE_250K
 #define NRF_CRC		RF24_CRC_16_BITS
 #define NRF_PA		RF24_PA_MIN
